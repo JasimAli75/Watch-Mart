ea989aa4224ee278d93444820ed5fe67f1c7fab7
Jasim Ali Rajput <115194580+JasimAli75@users.noreply.github.com>
2023-07-14 04:42:18 +0500

    Shipping break point

--- src/MainComponents/views/ShippingDetails/ShipDetails.tsx
+++ src/MainComponents/views/ShippingDetails/ShipDetails.tsx
@@ -11,24 +11,34 @@ function ShipDetails() {
         <AiOutlineArrowRight className="h-8 w-8 hover:scale-110 eas-In duration-300 " />
 
         <div>
-          <h1 className="text-lg font-semibold">30 days return policy</h1>
-          <p className="text-sm">Get your money back for any purchase</p>
+          <h1 className="text-lg font-semibold sm:flex hidden">
+            30 days return policy
+          </h1>
+          <p className="text-sm md:flex hidden">
+            Get your money back for any purchase
+          </p>
         </div>
       </div>
       {/* Free shipping */}
       <div className="flex items-center gap-3">
         <FaShippingFast className="h-8 w-8 hover:scale-110 eas-In duration-300" />
         <div>
-          <h1 className="text-lg font-semibold">Free express shipping </h1>
-          <p className="text-sm">Free shipping over $200</p>
+          <h1 className="text-lg font-semibold sm:flex hidden">
+            Free express shipping{" "}
+          </h1>
+          <p className="text-sm md:flex hidden">Free shipping over $200</p>
         </div>
       </div>
       {/* Secure payment */}
       <div className="flex items-center gap-3">
         <RiSecurePaymentLine className="h-8 w-8 hover:scale-110 eas-In duration-300" />
         <div>
-          <h1 className="text-lg font-semibold">Secure payment</h1>
-          <p className="text-sm">Clear pricing and fast payouts</p>
+          <h1 className="text-lg font-semibold sm:flex hidden">
+            Secure payment
+          </h1>
+          <p className="text-sm md:flex hidden">
+            Clear pricing and fast payouts
+          </p>
         </div>
       </div>
     </div>
