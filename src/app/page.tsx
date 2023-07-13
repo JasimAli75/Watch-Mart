6046991f82b9d681c0feb6414edaab01f7319671
Jasim Ali Rajput <115194580+JasimAli75@users.noreply.github.com>
2023-07-14 04:07:44 +0500

    components added
    shipping and newsletter components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Hero from "@/MainComponents/views/Hero/Hero";
 import ProductCategory from "@/MainComponents/views/ProductCategory/ProductCategory";
+import ShipDetails from "@/MainComponents/views/ShippingDetails/ShipDetails";
+import Newsletter from "@/MainComponents/views/Newsletter/Newsletter";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <ProductCategory />
+      <ShipDetails />
+      <Newsletter />
     </div>
   );
 }
