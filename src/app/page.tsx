3b7970ef9dc48b6c9f3a659e45b4f9dbff9057ba
Jasim Ali Rajput <115194580+JasimAli75@users.noreply.github.com>
2023-07-14 04:00:45 +0500

    Product Category added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Hero from "@/MainComponents/views/Hero/Hero";
+import ProductCategory from "@/MainComponents/views/ProductCategory/ProductCategory";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <ProductCategory />
     </div>
   );
 }
