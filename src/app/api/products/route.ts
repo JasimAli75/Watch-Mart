1939db36ac70d23237820fb1bd1149dd14f41b94
Jasim Ali Rajput <115194580+JasimAli75@users.noreply.github.com>
2023-07-14 16:24:24 +0500

    Sanity client added

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,4 +1,13 @@
 import { NextResponse } from "next/server";
+import { createClient } from "next-sanity";
+
+export const client = createClient({
+  token: `${process.env.SANITY_ACCESS_TOKEN}`,
+  dataset: `${process.env.NEXT_PUBLIC_SANITY_DATASET}`,
+  projectId: `${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}`,
+  apiVersion: "v2023-07-13",
+  useCdn: true,
+});
 
 export async function GET() {
   try {
