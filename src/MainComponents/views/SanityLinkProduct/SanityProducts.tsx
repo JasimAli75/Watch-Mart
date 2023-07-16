46641a9fe78e167a9e37fb2dc4dd16bdc6d93fe1
Jasim Ali Rajput <115194580+JasimAli75@users.noreply.github.com>
2023-07-17 03:17:19 +0500

    node module reinstall

--- src/MainComponents/views/SanityLinkProduct/SanityProducts.tsx
+++ src/MainComponents/views/SanityLinkProduct/SanityProducts.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { oneProductType } from "@/MainComponents/uitls/ProductsDataArrayAndType";
-import { FC } from "react";
-const SanityProducts = ({ ProductData }: any) => {
-  return <div>{ProductData[0].productName}</div>;
-};
-export default SanityProducts;
+// import { oneProductType } from "@/MainComponents/uitls/ProductsDataArrayAndType";
+// import { FC } from "react";
+// const SanityProducts = ({ ProductData }: any) => {
+//   return <div>{ProductData[0].productName}</div>;
+// };
+// export default SanityProducts;
 
 // : FC<{ ProductData: Array<oneProductType> }> = ({
 //   ProductData,
