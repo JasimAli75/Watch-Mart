f2a8bb23e37de2cd9df4408680821996b0343b35
Jasim Ali Rajput <115194580+JasimAli75@users.noreply.github.com>
2023-07-16 13:20:42 +0500

    sanity update

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,5 @@
 import { type SchemaTypeDefinition } from "sanity";
-import { product } from "./Product";
+import { product } from "./Product/Product";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [product],
