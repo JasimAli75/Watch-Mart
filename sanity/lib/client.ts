94590a34adc20ad2fbaa36745109f853eca80259
Jasim Ali Rajput <115194580+JasimAli75@users.noreply.github.com>
2023-07-20 16:57:48 +0500

    delete sanity

--- sanity/lib/client.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "next-sanity";
-
-const client = createClient({
-  projectId: "mv80qxh3",
-  dataset: "production",
-  apiVersion: "v2023-07-18",
-  useCdn: false,
-});
-export default client;
--- src/MainComponents/views/SanityProduct/SanityItems.tsx
+++ src/MainComponents/views/SanityProduct/SanityItems.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import client from "../../../../sanity/lib/client";
 import ProductCard from "./ProductCard";
 import { fetchTutorials } from "@/app/api/product/route";
 
