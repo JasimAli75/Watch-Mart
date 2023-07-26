df06b9c3f246825db74bed2808524b9d7fb3f0f6
Jasim Ali Rajput <115194580+JasimAli75@users.noreply.github.com>
2023-07-27 02:14:18 +0500

    Sanity delete
    Sanity delete

--- sanity/schema.ts
+++
@@ -1,6 +0,0 @@
-import { type SchemaTypeDefinition } from "sanity";
-import Products from "./Schemas/Products";
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [Products],
-};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,16 @@ import PromoProduct from "@/MainComponents/views/PromoProducts";
 import BASE_PATH_FORAPI from "@/MainComponents/shared/BasePath";
 import ProductCarousel from "@/MainComponents/views/SanityProducts/ProductCarousel";
 
-async function fetchAllProductsData() {
-  let res = await fetch(`${BASE_PATH_FORAPI}/api/product`);
-  if (!res.ok) {
-    throw new Error("failed to fetch data ");
-  }
-  return res.json();
-}
+// async function fetchAllProductsData() {
+//   let res = await fetch(`${BASE_PATH_FORAPI}/api/product`);
+//   if (!res.ok) {
+//     throw new Error("failed to fetch data ");
+//   }
+//   return res.json();
+// }
 export default async function Home() {
-  let { response } = await fetchAllProductsData();
-  console.log("response : ", response);
+  // let { response } = await fetchAllProductsData();
+  // console.log("response : ", response);
 
   return (
     <div>
